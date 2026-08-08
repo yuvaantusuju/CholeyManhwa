3842331e69de62329d4d14f2ed296d1923d93251
yuvaantusuju <ytusuju@gmail.com>
2026-08-08 14:28:59 +0530

    Update route.ts

--- app/api/parse/route.ts
+++ app/api/parse/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import { NextResponse } from 'next/server';
 import * as cheerio from 'cheerio';
 import { absoluteUrl, assertSafeRemoteUrl, fetchPage } from '../../lib/scraper';
